8bca1dd1b25a7c40f529d66bded71b16da66c27c
Alex Bzdel <abzdel25@gmail.com>
2022-09-17 22:02:37 +0000

    added more testing

--- dblib/cli_extension.py
+++ dblib/cli_extension.py
@@ -1,7 +1,9 @@
 #!./venv/bin/python
 
+import sys
 import click
-from querydb import querydb
+from querydb import querydb, custom_query
+
 
 # build a click group
 @click.group()
@@ -13,13 +15,42 @@ def cli():
 @cli.command()
 @click.option(
     "--query",
-    default="SELECT * FROM default.diamonds LIMIT 2",
+    # defaults to selecting the top two rows from fortune1000 table
+    default="SELECT * FROM default.fortune_1000 LIMIT 2",
     help="SQL query to execute",
 )
 def cli_query(query):
-    """Execute a SQL query"""
+    """Execute a SQL query."""
     querydb(query)
 
+
+@cli.command()
+@click.option(
+    "--rank",
+    # defaults to selecting the first company (rank 1)
+    # should be Walmart in our dataset
+    default="SELECT name FROM default.fortune_1000 WHERE rank = 1 LIMIT 1",
+    help="SQL query to execute",
+)
+
+def rank_query(rank):
+    """
+    Query fortune1000 table based on the rank of a company.
+    Takes rank as a command line argument.
+    Calls custom_query from querydb.py.
+
+    """
+
+    if not len(sys.argv) == 4:
+        print("must enter ONE integer value when using --rank")
+        exit()
+    if not sys.argv[3].isdigit():
+        print("must enter an integer value for rank")
+        exit()
+
+    rank = int(sys.argv[3])
+
+    custom_query(rank)
 
 # run the CLI
 if __name__ == "__main__":
--- dblib/querydb.py
+++ dblib/querydb.py
@@ -4,16 +4,44 @@ import os
 from databricks import sql
 
 def querydb(query):
+    """
+    Default query, selects top two rows from fortune100 table.
+    Returns result from our query.
+    """
 
     with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
                  http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
                  access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
 
         with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM default.fortune1000 LIMIT 2")
+            cursor.execute("SELECT * FROM default.fortune_1000 LIMIT 2")
             result = cursor.fetchall()
 
         for row in result:
             print(row)
+
+    return result
+
+
+
+def custom_query(rank):
+    """
+    Custom query - user enters a rank (for fortune 1000).
+    Returns and prints company associated with rank.
+    """
+
+    assert rank < 1001
+    assert rank > 0
+
+    with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                 http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
+                 access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
+
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT name FROM default.fortune_1000 WHERE rank = {rank} LIMIT 1")
+            result = cursor.fetchone()
+
+    print(f"{result[0]} is rank {rank} in our dataset.")
+
 
     return result
